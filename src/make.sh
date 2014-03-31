5cda425073b6eff9824d5133e5933df03c6bd1d5
Pernod70 <ngbarnes@hotmail.com>
2014-03-31 01:20:29 +0100

    Updates for DSM 5.0 - Removed Web Station check.

--- src/make.sh
+++ src/make.sh
@@ -4,8 +4,8 @@ chown -R root:root desktop navphp ui
 chmod -R 755 *
 tar -czf package.tgz -X exclude.txt desktop navphp ui
 
-chown -R root:root scripts CHANGELOG INFO package.tgz PACKAGE_ICON.PNG
+chown -R root:root scripts CHANGELOG INFO package.tgz PACKAGE_ICON.PNG PACKAGE_ICON_120.PNG
 chmod -R 755 *
-tar -cvf ../PHPNavigator-4.44-001.spk -X exclude.txt scripts CHANGELOG INFO package.tgz PACKAGE_ICON.PNG
+tar -cvf ../PHPNavigator-4.44-002.spk -X exclude.txt scripts CHANGELOG INFO package.tgz PACKAGE_ICON.PNG PACKAGE_ICON_120.PNG
 
 exit 0
